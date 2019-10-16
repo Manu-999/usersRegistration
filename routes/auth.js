2771416522d8e4136dcce909528d5fe6c729cd3f
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-16 15:22:41 +0200

    validation added and checking if username and email is already registered feature working

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,37 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const { registerValidation } = require('../validation');
 
-router.post('/register', (req, res) => {
-  res.send('User registered');
+router.post('/register', async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Checking if a user is already registered
+
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist)
+    return res.status(400).send('This email has been already registered!');
+
+  // Checking if username is already registered
+
+  const userNameExist = await User.findOne({ username: req.body.username });
+  if (userNameExist)
+    return res.status(400).send('This username is already taken!');
+
+  // Creating a new User
+
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;

4925d4383d123058433e737d872c13470a0ecbdc
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-18 18:30:41 +0200

    aws s3 file upload

--- routes/files.js
+++ routes/files.js
@@ -6,26 +6,36 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const bodyParser = require('body-parser');
 
-aws.config.update({
-  secretAccessKey: 'cPf5pyX8m/PAcqsrw3RLs0rM1Kt67MNnOaOZDtF1',
-  accessKeyId: 'AKIAY74DF3JTKB2AGMWV'
-});
-
 app.use(bodyParser.json());
 
 var upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'bucket-name',
+    bucket: 'aluxion.bucket/tests',
     key: function(req, file, cb) {
       console.log(file);
-      cb(null, file.originalname); //use Date.now() for unique file keys
+      cb(null, file.originalname);
     }
   })
 });
 
-router.get('/', (req, res) => {
-  res.render('uploadFile.ejs');
+router.get('/', async (req, res) => {
+  try {
+    aws.config.setPromisesDependency();
+    aws.config.update({
+      secretAccessKey: 'cPf5pyX8m/PAcqsrw3RLs0rM1Kt67MNnOaOZDtF1',
+      accessKeyId: 'AKIAY74DF3JTKB2AGMWV'
+    });
+    const response = await s3
+      .listObjectsV2({
+        Bucket: 'aluxion.bucket/tests'
+      })
+      .promise();
+    console.log(response);
+    res.render('uploadFile.ejs');
+  } catch (e) {
+    console.log('our error', e);
+  }
 });
 
 router.post('/upload', upload.array('file', 1), (req, res, next) => {
